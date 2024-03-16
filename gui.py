017acf357bf7fb0a7c03ed65835fdae18a01c3af
Jaysterr <j4ysterr@gmail.com>
2024-03-16 14:32:45 -0700

    even more changes

--- gui.py
+++ gui.py
@@ -1,20 +1,27 @@
-from nicegui import ui
+from nicegui import ui, Client
+
 from GameManager import *
 from GameManager import GameManager
+import asyncio
 
+input_fields = []
 @ui.page('/')
 def init_gui():
-    input_fields = []
+    
+    async def javascript_stuff():
+        ui.run_javascript(f"document.getElementById({input_fields[3].id}).select()")
+    
+    
     with ui.tabs().classes('w-full') as tabs:
         standard = ui.tab("Standard Mode")
         not_standard = ui.tab("Not Standard Mode")
         
     with ui.tab_panels(tabs, value=standard).classes('w-full'):
         with ui.tab_panel(standard).classes("items-center"):
-            with ui.row(wrap=False):
+            with ui.row(wrap=False).classes("content-center"):
                 for i in range(5):
-                    input_fields.append(ui.input().classes("w-1/5 text-2xl").props('filled'))
-                    
+                    input_fields.append(ui.input().classes("w-1/6 text-2xl").props('input-class="text-center" filled'))
+            ui.button(on_click=javascript_stuff)
             textfield = ui.input("enter a word here!").classes("object-center")
             ui.button("Click to submit answer", on_click=lambda: label.set_text("You typed: " + textfield.value))
             label = ui.label()
@@ -28,25 +35,26 @@ def init_gui():
     ui.button("Click to submit answer", on_click=lambda: label.set_text("You typed: " + textfield.value))
     label = ui.label()
     label2 = ui.label()
-    with ui.left_drawer(top_corner=True, bottom_corner=True):
-        ui.label("left")
+    # with ui.left_drawer(top_corner=True, bottom_corner=True):
+    #     ui.label("left")
 
-    ui.add_head_html(r'''
-    <style>
-    @keyframes fade {
-    from {opacity: 0;}
-    to {opacity: 1.0;}
-    }
-    </style>
-    ''')
+    # ui.add_head_html(r'''
+    # <style>
+    # @keyframes fade {
+    # from {opacity: 0;}
+    # to {opacity: 1.0;}
+    # }
+    # </style>
+    # ''')
 
     ui.label('Hello world!').style('animation: fade 3s')
     # ui.timer(0.001, lambda: label2.set_text("{0:.3f}s".format(game.get_time_elapsed() / (10**9)))) # alt timer style
     ui.timer(0.001, lambda: label2.set_text(format_timer(game.get_time_elapsed() / (10**9))))
 
     ui.run(native=True)
-
 def format_timer(sec):
     ms = (sec % 1) * 1000
     s = sec // 1
     return str(int(s)) + "s " + "{0:02d}".format((int(ms//10))) #+ "ms"
+
+
