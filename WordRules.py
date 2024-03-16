d49e24a077c0b981822c9bcf89f5d18de3c42c23
Jakob Garcia <jakobg@arizona.edu>
2024-03-16 11:41:04 -0700

    Created word list

--- WordRules.py
+++ WordRules.py
@@ -8,11 +8,17 @@
 #    could be merged with GameManager if it turns out there isnt good enough reason to seperate
 
 # TODO: Implement WordRules
+import string
 class WordRules:
-    
-    
     def __init__(self):
-        pass
+        try:
+            file = open('words.txt')
+        except FileNotFoundError:
+            print('ERROR: File not found')
+        else:
+            self._word_list = [word.strip().lower() for word in file if len(word.strip()) == 5 and string.punctuation not in word]
+            file.close()
+
     
     def contains_letters(letters: list[str]):
         pass
